0e1b80623dd87bb7b204ffcb09e733b6cdbd492a
prabhat1510 <prabhatc@gmail.com>
2022-02-03 17:40:34 +0530

    my first helloworld java project from eclipse

---
+++ helloworlddemo/src/helloworlddemo/main/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package helloworlddemo.main;
+
+/**
+ * @author UD SYSTEMS
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//syso then ctrl-space
+		System.out.println("Hello Everyone Welcome to eclipse");
+
+	}
+
+}
